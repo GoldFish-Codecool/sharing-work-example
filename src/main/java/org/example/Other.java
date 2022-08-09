97309a479d122c318e95432955db48ba7fbea97a
DESKTOP-5HJ9E7H\Nyila <gabor.nyilas@codecool.com>
2022-08-09 13:25:57 +0200

    add DoubleNumber() method to Other.java

--- src/main/java/org/example/Other.java
+++ src/main/java/org/example/Other.java
@@ -3,4 +3,9 @@ package org.example;
 public class Other {
     String Something;
     Integer Number;
+
+    public Integer DoubleNumber() {
+        Number = Number * 2;
+        return Number;
+    }
 }
